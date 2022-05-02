5acfb037823a7ff68c6484b2fec1d314596730b6
Leshukov Viktor <leshukov.broccoligames@gmail.com>
2022-05-02 14:31:21 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialRentAccunting.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
